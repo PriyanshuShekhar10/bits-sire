96e3749b8e0744b611480691e4bbfa5a314175a4
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2024-03-06 13:03:59 +0530

    Added Header

---
+++ src/Divider.js
@@ -0,0 +1,17 @@
+const Divider = () => {
+  return (
+    <div
+      style={{
+        height: "2px",
+        backgroundColor: "#C12872",
+        marginTop: "20px",
+        marginBottom: "20px",
+        width: "75%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    ></div>
+  );
+};
+
+export default Divider;
