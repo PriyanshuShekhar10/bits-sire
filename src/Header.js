05186fbb591314c6280899bf56b92bf27616bd6f
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2024-03-07 14:19:51 +0530

    Add: many changes

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./left.png"; // Adjust the path based on your folder structure
+// import logo from "./left.png"; // Adjust the path based on your folder structure
 import styles from "./Header.module.css"; // Import the CSS module
 
 function Header({ title }) {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="Logo" className={styles.logo} />
+      {/* <img src={logo} alt="Logo" className={styles.logo} /> */}
       <h1 className={styles.title}>{title}</h1>
     </header>
   );
--- src/TeamMember.js
+++ src/TeamMember.js
@@ -1,7 +1,10 @@
-// TeamMember.js
 import React from "react";
 
 const TeamMember = ({ name, title, imageSrc, profileUrl, role }) => {
+  // Check if the title contains a pipe '|' character and split accordingly
+  const titleContainsPipe = title.includes("|");
+  const titleParts = titleContainsPipe ? title.split(/\|\s*(.+)/) : [title];
+
   return (
     <div className="team-member">
       <img
@@ -11,15 +14,24 @@ const TeamMember = ({ name, title, imageSrc, profileUrl, role }) => {
       />
       <h3 className="team-member__name">
         <a
+          href={profileUrl}
           target="_blank"
           rel="noopener noreferrer"
           style={{ color: "black", textDecoration: "none" }}
-          href={profileUrl}
         >
           {name}
         </a>
       </h3>
-      <p className="team-member__title">{title}</p>
+      <p className="team-member__title">
+        {titleParts[0]}
+        {/* Render the second part with a line break if a pipe '|' was present in the title */}
+        {titleContainsPipe && titleParts[1] && (
+          <>
+            <br />
+            {titleParts[1]}
+          </>
+        )}
+      </p>
       <p className="team-member__title2">{role}</p>
     </div>
   );
