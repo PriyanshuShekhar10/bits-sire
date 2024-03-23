a1ff333df8fd6df19af3dd42bd7d5c60b21dcae7
PriyanshuShekhar10 <62722200+PriyanshuShekhar10@users.noreply.github.com>
2024-03-23 15:10:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ const teamMembers2 = [
     name: "Mr. Manoj Chulliparambil",
     profileUrl: "",
     title: "Head – Strategy |New Areas & Knowledge Centre, ABSTCPL",
-    imageSrc: "https://i.ibb.co/r5MgCcB/unnamed.jpg",
+    imageSrc: "https://i.ibb.co/wwgjtHs/unnamed.jpg",
     role: "Member",
   },
   {
